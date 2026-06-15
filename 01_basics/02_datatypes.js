9c9077ea7c3bb19bcc98208d11acf0f87cd0ac17
RISHI PAWAR <161636122+rishipawar0025@users.noreply.github.com>
2026-06-15 11:49:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"user strict"; //treat all JS code as a newer version
+
+// alert (3+3) //we are using nodejs, not browser
+
+console.log(3 
+    +
+    3
+)     // code readability should be high
+
+ console.log("Rishi")
+
+ let name = "Rishi"
+ let age = 21
+ let isLoggedIn = false
+
+ let State;
+
+ //number => 2 to power 53
+ //bigint
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ // undefined =>
+// symbol => unique
+
+ //object
+ console.log(typeof undefined); //undefined
+ console.log(typeof State); //unique
+ 
