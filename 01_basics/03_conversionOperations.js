bffbe000ce1f401dbbad4c58016552850dbb5a2c
RISHI PAWAR <161636122+rishipawar0025@users.noreply.github.com>
2026-06-17 11:41:46 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score="rishi"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" =>33
+//"33abc" => NaN
+//true => 1; false => 0.
+
+let isLoggedIn = "Rishi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Rishi"=> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
